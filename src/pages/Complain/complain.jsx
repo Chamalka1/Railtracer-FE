7def45aa52c67e494c63eb80cd28a798a59ea90a
Diluka Fernado <dsandamali47@gmail.com>
2025-03-30 13:29:36 +0530

    complain.jsx defined

--- src/pages/Complain/complain.jsx
+++ src/pages/Complain/complain.jsx
@@ -2,63 +2,63 @@ import React from "react";
 
 export const Complain = () => {
   return (
-    <div class="w-50">
-      <div class="mb-3">
+    <div className="w-50">
+      <div className="mb-3">
         <h4>Complains</h4>
       </div>
 
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label"></label>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="date"
         />
       </div>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label"></label>
 
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="name"
         />
       </div>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label"></label>
 
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="ID no"
         />
       </div>
 
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"></label>
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label"></label>
 
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="Package No"
         />
       </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
+      <div className="mb-3">
+        <label for="exampleFormControlTextarea1" className="form-label">
           Description
         </label>
         <textarea
-          class="form-control"
+          className="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
         ></textarea>
       </div>
 
-      <button type="button" class="btn btn-primary">
+      <button type="button" className="btn btn-primary">
         Submit
       </button>
     </div>
--- src/pages/GoodRetrever/GoodRetreve.jsx
+++ src/pages/GoodRetrever/GoodRetreve.jsx
@@ -2,97 +2,97 @@ import React from "react";
 
 export const GoodRetreve = () => {
   return (
-    <div class="w-50 m-3">
+    <div className="w-50 m-3">
       <div>
-        <h4 class="mb-4">Good Retrever</h4>
+        <h4 className="mb-4">Good Retrever</h4>
       </div>
       <div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
             Name
           </label>
           <input
             type="Name"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="name"
           ></input>
         </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
             Email address
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="name@example.com"
           ></input>
         </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
             Address
           </label>
           <input
             type="Name"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Adress"
           ></input>
         </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
             Telephon No
           </label>
           <input
             type="Name"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Telephone No"
           ></input>
         </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
             ID NO
           </label>
           <input
             type="Name"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="ID no"
           ></input>
         </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
             Traking no
           </label>
           <input
             type="Name"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Traking No"
           ></input>
         </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">
             {" "}
             Goods discription
           </label>
           <textarea
-            class="form-control"
+            className="form-control"
             id="exampleFormControlTextarea1"
             rows="Goods discription"
           ></textarea>
         </div>
 
-        <div class="mb-5">
-          <input class="btn btn-primary" type="submit" value="Submit" />
+        <div className="mb-5">
+          <input className="btn btn-primary" type="submit" value="Submit" />
         </div>
       </div>
     </div>
